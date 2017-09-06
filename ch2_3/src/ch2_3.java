bd74e0ae13d0f7d5b06890e70275cb1f88f739c1
lucas <lucas@asia.edu.tw>
2017-09-06 15:38:30 +0800

    c2

---
+++ ch2_3/src/ch2_3.java
@@ -0,0 +1,18 @@
+/**
+ * Created by lucas on 2017/7/14.
+ */
+public class ch2_3 {
+    public static void main(String[] args){
+       java.util.Scanner sc=new java.util.Scanner(System.in);
+       String name;
+       System.out.print("請輸入姓名：");
+       name=sc.nextLine();
+       System.out.print("請輸入成績");
+       int score;
+       score=sc.nextInt();
+       System.out.println("你的姓名為"+name+",成績為"+score+"分。");
+
+         }
+
+}
+
---
+++ src/ch2_1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lucas on 2017/7/14.
+ */
+public class ch2_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("第1個java程式!");
+    }
+}
